675c1d47d3f15caee7c7cadc1d11a8626a0ae0b7
Kirill Stepanov <iostepanovk@gmail.com>
2019-07-24 16:45:59 +0500

    Добавьте файлы проекта.

---
+++ JsExample/wwwroot/examplejs.js
@@ -0,0 +1,9 @@
+﻿window.ShowWhatIKnow = (name) => {
+    if (name === "Jon Snow") {
+        return "You know nothing, " + name;
+    }
+    else {
+        return "Maybe, you know something, " + name;
+    }
+}
+
